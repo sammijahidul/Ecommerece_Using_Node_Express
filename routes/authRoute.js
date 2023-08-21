c1cbcf064844f8e0cc9e67c5a99b668791a8c426
sammijahidul <followjahidul@gmail.com>
2023-08-21 16:47:02 +0600

    login functionality & jwt used for secure routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,17 @@
 import express from "express";
-import {registerController} from '../controllers/authController.js';
+import {registerController, loginController, testController} from '../controllers/authController.js';
+import {requireSignIn} from '../middlewares/authMiddleware.js';
 
 //router object
 const router = express.Router();
 
+//Register
 router.post('/register', registerController)
 
-export default router;
+//login 
+router.post('/login', loginController);
+
+//test routes
+router.get('/test', requireSignIn, testController)
+
+export default router; 
