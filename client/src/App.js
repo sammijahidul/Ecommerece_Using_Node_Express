8dba4f8bdcb8286f0d19fdd6a1315e68e766bec1
sammijahidul <followjahidul@gmail.com>
2023-08-24 01:27:13 +0600

    login & logout functionality completed

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,17 @@ import Contact from './pages/Contact';
 import Policy from './pages/Policy';
 import PagenotFound from './pages/PagenotFound';
 import Register from './pages/Auth/Register';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
 
 function App() {
   return (
     <>
       <Routes>
           <Route path='/' element={<HomePage />} />
+          <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/policy' element={<Policy />} />
--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import {NavLink,Link} from 'react-router-dom';
 import {HiOutlineShoppingCart} from 'react-icons/hi';
+import { useAuth } from '../../context/auth';
+import toast from 'react-hot-toast';
 
 const Header = () => {
+    const [auth,setAuth] = useAuth();
+    const handleLogout = () => {
+      setAuth({
+        ...auth,
+        user: null,
+        token: ""
+      })
+      localStorage.removeItem('auth');
+      toast.success("Logout Successfully done");
+    }
     return (
         <>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -17,27 +29,43 @@ const Header = () => {
                 <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                <Link to="/" className="navbar-brand"> <HiOutlineShoppingCart/> Ecommerce App
+                <Link to="/" className="navbar-brand"> <HiOutlineShoppingCart/> 
+                Ecommerce App
                 </Link>
                 <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                      <NavLink to="/" className="nav-link" >Home
+                      <NavLink to="/" className="nav-link" >
+                        Home
                       </NavLink>
                     </li>
                     <li className="nav-item">
-                      <NavLink to="/category" className="nav-link" >Category
+                      <NavLink to="/category" className="nav-link" >
+                        Category
                       </NavLink>
                     </li>
+                    {
+                      !auth.user ? (<>
+                      <li className="nav-item">
+                        <NavLink to="/register" className="nav-link">
+                          Register
+                        </NavLink>
+                      </li>
+                      <li className="nav-item">
+                        <NavLink to="/login" className="nav-link" >
+                          Login
+                        </NavLink>
+                      </li>
+                      </>)  :  (<>
+                        <li className="nav-item">
+                          <NavLink onClick={handleLogout} to="/login" className="nav-link" >
+                            Logout
+                          </NavLink>
+                        </li>
+                      </>)
+                    }
                     <li className="nav-item">
-                      <NavLink to="/register" className="nav-link">Register
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to="/login" className="nav-link" >Login
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink to="/cart" className="nav-link" >Cart (0)
+                      <NavLink to="/cart" className="nav-link" >
+                        Cart (0)
                       </NavLink>
                     </li>
                     
